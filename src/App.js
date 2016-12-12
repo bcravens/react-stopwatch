576ba50d01c7d7c1aa1939cd2e8d55aa212b7dc9
Bradley Cravens <bradleycravens@gmail.com>
2016-12-12 11:59:38 -0500

    Bradley Cravens StopWatch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+class Stopwatch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      secondsElapsed: 0
+    };
+
+    this.handleStartClick = this.handleStartClick.bind(this);
+    this.handleStopClick = this.handleStopClick.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+  }
+
+  getSeconds() {
+    return ('0' + this.state.secondsElapsed % 60).slice(-2);
+  }
+
+  getMinutes() {
+    return Math.floor(this.state.secondsElapsed / 60);
+  }
+
+  handleStartClick() {
+    this.increment = setInterval( () => {
+      this.setState({ secondsElapsed: (this.state.secondsElapsed + 1)});
+    }, 1000)
+  }
+
+  handleStopClick() {
+    clearInterval(this.increment);
+  }
+
+  handleResetClick() {
+    clearInterval(this.increment);
+    this.setState({ secondsElapsed: 0});
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <h1>{this.getMinutes()}:{this.getSeconds()}</h1>
+      <button type="button" onClick={this.handleStartClick}>Start</button>
+      <button type="button" onClick={this.handleStopClick}>Stop</button>
+      <button type="button" onClick={this.handleResetClick}>Reset</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default Stopwatch;
